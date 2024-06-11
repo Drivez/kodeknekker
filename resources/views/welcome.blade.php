b2583557e67028b9910edaa49c6e555d86c13a5a
Marius Kildedal <marius.kildedal@atea.no>
2024-06-11 15:16:20 +0200

    Update image source path for logo in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
     <section class="container mx-auto px-6 h-full flex flex-col" style="max-width: 1000px;">
         <header class="flex">
             <h1>
-                <img src="/kodeknekker.png" alt="Kodeknekker" aria-label="Kodeknekker" class="print:invert w-36 print:w-36">
+                <img src="kodeknekker.png" alt="Kodeknekker" aria-label="Kodeknekker" class="print:invert w-36 print:w-36">
             </h1>
         </header>
 
